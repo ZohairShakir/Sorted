9bd09aced4695d174eb73b780b679e07a7a6c25e
Zohair Shakir <zohairshakir2007@gmail.com>
2025-07-28 09:36:17 +0530

    render deployment

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def download():
         return "File not found", 404
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=5000)
